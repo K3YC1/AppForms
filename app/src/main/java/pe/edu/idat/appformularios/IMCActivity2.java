8d9ac235d889d4227d94136b94a5c920fcfe34d2
unknown <you@example.com>
2023-10-02 18:45:31 -0500

    first commit

---
+++ app/src/main/java/pe/edu/idat/appformularios/IMCActivity2.java
@@ -0,0 +1,14 @@
+package pe.edu.idat.appformularios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IMCActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imcactivity2);
+    }
+}
