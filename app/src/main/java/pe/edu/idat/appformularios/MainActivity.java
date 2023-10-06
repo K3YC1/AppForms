d7fe0fe4540d6e5b0457db3fdbf2601ede693dd9
Keyci <keycirosales@gmail.com>
2023-10-05 21:23:13 -0500

    formularios suma e IMC

--- app/src/main/java/pe/edu/idat/appformularios/MainActivity.java
+++ app/src/main/java/pe/edu/idat/appformularios/MainActivity.java
@@ -3,12 +3,32 @@ package pe.edu.idat.appformularios;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import pe.edu.idat.appformularios.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.btnCalcular.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Integer numero1 = Integer.parseInt(binding.txtnumero1.getText().toString());
+        Integer numero2 = Integer.parseInt(binding.txtnumero2.getText().toString());
+        Integer suma = sumar(numero1, numero2);
+        binding.tvresultadosuma.setText("La suma es = " + suma);
+    }
+
+    private Integer sumar(Integer numero1, Integer numero2){
+        return numero1 + numero2;
     }
 }
